c2df8887e18ff91692cad3fb8a5e84df3c8193e2
Joeny Vo <jvo1563@gmail.com>
2023-05-06 22:50:28 -0700

    added db to backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 // https://expressjs.com/en/resources/middleware/morgan.html
 const morgan = require("morgan");
-const app = express();
-
-app.use(express.static("build"));
-app.use(cors());
-app.use(express.json());
+require("dotenv").config();
+const Person = require("./models/person");
 
 morgan.token("postContent", (req, res) => {
   if (req.method === "POST") {
@@ -14,61 +12,63 @@ morgan.token("postContent", (req, res) => {
   }
   return "";
 });
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+app.use(cors());
+app.use(express.json());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :postContent"
   )
 );
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(express.static("build"));
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<div><div>Phonebook has info for ${
-      persons.length
-    } people</div><div>${new Date()}</div></div>`
-  );
+  Person.find({}).then((persons) => {
+    response.send(
+      `<div><div>Phonebook has info for ${
+        persons.length
+      } people</div><div>${new Date()}</div></div>`
+    );
+  });
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -83,24 +83,48 @@ app.post("/api/persons", (request, response) => {
       error: "number missing",
     });
   }
-  const checkExist = persons.find((person) => person.name === body.name);
-  if (checkExist) {
+  // const checkExist = persons.find((person) => person.name === body.name);
+  // if (checkExist) {
+  //   return response.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  if (!body.name) {
     return response.status(400).json({
-      error: "name must be unique",
+      error: "name missing",
     });
   }
-
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
   const person = {
-    id: Math.floor(Math.random() * 1000000 + 1),
     name: body.name,
     number: body.number,
   };
-
-  persons = persons.concat(person);
-  response.json(person);
+  Person.findByIdAndUpdate(request.params.id, person, { new: true }) // without new: true the updatedPerson would be the old original note
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-const PORT = 3001;
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
