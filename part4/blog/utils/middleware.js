d7fd358ac91e8eb9ca1159e0fb44e2389d4c2dc1
Joeny Vo <jvo1563@gmail.com>
2023-05-09 21:19:34 -0700

    finished 4.2

---
+++ part4/blog/utils/middleware.js
@@ -0,0 +1,30 @@
+const logger = require("./logger");
+const morgan = require("morgan");
+
+morgan.token("postContent", (req) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  morgan,
+  unknownEndpoint,
+  errorHandler,
+};
