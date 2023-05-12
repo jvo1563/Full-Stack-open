911fd1c27de4f1f64840dbde8e4757aca5ff7bec
Joeny Vo <jvo1563@gmail.com>
2023-05-11 22:59:13 -0700

    4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,8 +1,13 @@
 const dummy = (blogs) => {
-  console.log(blogs);
+  //   console.log(blogs);
   return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
 module.exports = {
   dummy,
+  totalLikes,
 };
