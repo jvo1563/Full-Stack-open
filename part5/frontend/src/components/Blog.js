b2e12b47fd14c04190310ba014589af746939cf1
Joeny Vo <jvo1563@gmail.com>
2023-05-25 20:49:47 -0700

    5.7

--- part5/frontend/src/components/Blog.js
+++ part5/frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogDetails = () => (
+    <div>
+      <div>{blog.url}</div>
+      <div>
+        Likes: {blog.likes}{" "}
+        <button onClick={console.log("add a like")}>Like</button>
+      </div>
+      <div>Posted by: {blog.user.name}</div>
+    </div>
+  );
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "Hide" : "View"}
+        </button>
+      </div>
+      {showDetails && blogDetails()}
+    </div>
+  );
+};
+
+export default Blog;
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: "solid",
+  borderWidth: 1,
+  marginBottom: 5,
+};
