79dc9eb789a85ae881f89e46f7e93a0bbe83a5d4
Joeny Vo <jvo1563@gmail.com>
2023-05-27 20:17:32 -0700

    5.10

--- part5/frontend/src/App.js
+++ part5/frontend/src/App.js
@@ -16,7 +16,10 @@ const App = () => {
   const blogFormRef = useRef();
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    blogService.getAll().then((blogs) => {
+      blogs.sort((a, b) => b.likes - a.likes);
+      setBlogs(blogs);
+    });
   }, []);
 
   useEffect(() => {
