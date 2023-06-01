eb16782adb81394e5d48430c2f10de9bea974717
Joeny Vo <jvo1563@gmail.com>
2023-05-31 21:30:49 -0700

    5.14

--- part5/frontend/src/components/Blog.test.js
+++ part5/frontend/src/components/Blog.test.js
@@ -1,22 +1,44 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("renders blog's title and author", () => {
+describe("<Blog />", () => {
+  let container;
   const blog = {
     title: "how to code",
     author: "Bob",
     url: "www.youtube.com",
+    likes: 0,
+    user: { name: "Poster", username: "admin" },
   };
 
-  render(<Blog blog={blog} />);
+  beforeEach(() => {
+    container = render(
+      <Blog blog={blog} user={{ username: "admin" }} />
+    ).container;
+  });
 
-  const title = screen.getByText("how to code", { exact: false });
-  const author = screen.getByText("bob", { exact: false });
-  const url = screen.queryByText("www.youtube.com");
+  test("renders blog's title and author", () => {
+    const title = screen.getByText("how to code", { exact: false });
+    const author = screen.getByText("bob", { exact: false });
+    const url = screen.queryByText("www.youtube.com");
 
-  expect(title).toBeDefined();
-  expect(author).toBeDefined();
-  expect(url).toBeNull();
+    expect(title).toBeDefined();
+    expect(author).toBeDefined();
+    expect(url).toBeNull();
+  });
+
+  test("renders blog's url and likes when button is clicked", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("View");
+    await user.click(button);
+
+    const url = screen.getByText("www.youtube.com", { exact: false });
+    const likes = screen.getByText("Likes:", { exact: false });
+
+    expect(url).toBeDefined();
+    expect(likes).toBeDefined();
+  });
 });
