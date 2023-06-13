d039b1c61d6fd2fe689e4219bf08b100740c6c59
Joeny Vo <jvo1563@gmail.com>
2023-06-12 19:23:19 -0700

    6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -16,7 +16,13 @@ const Anecdote = ({ anecdote, handleVote }) => {
 const AnecdotesList = () => {
   const dispatch = useDispatch();
   const anecdotes = useSelector((state) =>
-    state.sort((a, b) => b.votes - a.votes)
+    state.filter
+      ? state.anecdotes
+          .filter((anecdote) =>
+            anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+          )
+          .sort((a, b) => b.votes - a.votes)
+      : state.anecdotes.sort((a, b) => b.votes - a.votes)
   );
   return (
     <ul>
